6e91ddb5ffd8cc0f261d3993495bf4ba30e2b737
lingxueli <ll2871@columbia.edu>
2019-09-29 15:51:29 -0600

    add primary key

--- src/main/java/com/example/bootstrap/Dog.java
+++ src/main/java/com/example/bootstrap/Dog.java
@@ -4,12 +4,28 @@ package com.example.bootstrap;
 import org.springframework.context.annotation.Bean;
 
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 
 @Entity
 public class Dog {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long id;
+
     private String Name;
     private String breed;
     private String origin;
+
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
 
     public String getName() {
         return Name;
