35ea9ae6b6427a6cf42ec60d259dcb440b3894d5
lingxueli <ll2871@columbia.edu>
2019-09-30 23:20:15 -0600

    add controller methods for services

--- src/main/java/com/example/bootstrap/web/APIQueryController.java
+++ src/main/java/com/example/bootstrap/web/APIQueryController.java
@@ -1,7 +1,50 @@
 package com.example.bootstrap.web;
 
+import com.example.bootstrap.entity.Dog;
+import com.example.bootstrap.service.DogService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 public class APIQueryController {
+
+    private DogService dogService;
+
+    @Autowired
+    public void setDogService(DogService dogService) {
+        this.dogService = dogService;
+    }
+
+    @GetMapping("/dog")
+    public ResponseEntity<List<Dog>> getAllDogs() {
+        List<Dog> list = dogService.retrieveDogs();
+        return new ResponseEntity<List<Dog>>(list, HttpStatus.OK);
+    }
+
+    @GetMapping("/breeds")
+    public ResponseEntity<List<String>> getAllBreeds() {
+        List<String> list = dogService.retrieveDogBreed();
+        return new ResponseEntity<List<String>>(list, HttpStatus.OK);
+    }
+
+    @GetMapping("/searchById")
+    public ResponseEntity<String> getBreedById(@RequestParam(required = true) long id) {
+        String breed = dogService.retrieveDogBreedById(id);
+        return new ResponseEntity<String>(breed, HttpStatus.OK);
+    }
+
+    @GetMapping("/names")
+    public ResponseEntity<List<String>> getAllNames() {
+        List<String> list = dogService.retrieveDogNames();
+        return new ResponseEntity<List<String>>(list, HttpStatus.OK);
+    }
+
+
+
 }
