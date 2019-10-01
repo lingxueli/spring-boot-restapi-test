00fd7b6cbf4ebc0242e8b23c1969e51234b8c30b
lingxueli <ll2871@columbia.edu>
2019-09-30 23:37:01 -0600

    add sample data

---
+++ src/main/resources/data.sql
@@ -0,0 +1,6 @@
+INSERT INTO dog (name, breed, origin) VALUES ('Bella','Golden Retriever','UK');
+INSERT INTO dog (name, breed, origin) VALUES ('Lucy','Havanese','Cuba');
+INSERT INTO dog (name, breed, origin) VALUES ('Daisy','Hokkaido','Japan');
+INSERT INTO dog (name, breed, origin) VALUES ('Luna','Japanese Spitz','Japan');
+INSERT INTO dog (name, breed, origin) VALUES ('Lola','Bichon Frisé','Spain');
+INSERT INTO dog (name, breed, origin) VALUES ('Sadie','Labrador Husky','Canada');
