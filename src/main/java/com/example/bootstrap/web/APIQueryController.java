21bbc69f4290e03c925b505acfd49e4cc88c113e
lingxueli <ll2871@columbia.edu>
2019-10-01 22:55:03 -0600

    fix SQL error

--- src/main/java/com/example/bootstrap/web/APIQueryController.java
+++ src/main/java/com/example/bootstrap/web/APIQueryController.java
@@ -2,6 +2,7 @@ package com.example.bootstrap.web;
 
 import com.example.bootstrap.entity.Dog;
 import com.example.bootstrap.service.DogService;
+import com.example.bootstrap.service.DogServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,6 +1,6 @@
-INSERT INTO dog (name, breed, origin) VALUES ('Bella','Golden Retriever','UK');
-INSERT INTO dog (name, breed, origin) VALUES ('Lucy','Havanese','Cuba');
-INSERT INTO dog (name, breed, origin) VALUES ('Daisy','Hokkaido','Japan');
-INSERT INTO dog (name, breed, origin) VALUES ('Luna','Japanese Spitz','Japan');
-INSERT INTO dog (name, breed, origin) VALUES ('Lola','Bichon Frisé','Spain');
-INSERT INTO dog (name, breed, origin) VALUES ('Sadie','Labrador Husky','Canada');
+INSERT INTO dog (id, name, breed, origin) VALUES (1, 'Bella','Golden Retriever','UK');
+INSERT INTO dog (id, name, breed, origin) VALUES (2, 'Lucy','Havanese','Cuba');
+INSERT INTO dog (id, name, breed, origin) VALUES (3, 'Daisy','Hokkaido','Japan');
+INSERT INTO dog (id, name, breed, origin) VALUES (4, 'Luna','Japanese Spitz','Japan');
+INSERT INTO dog (id, name, breed, origin) VALUES (5, 'Lola','Bichon Frisé','Spain');
+INSERT INTO dog (id, name, breed, origin) VALUES (6, 'Sadie','Labrador Husky','Canada');
