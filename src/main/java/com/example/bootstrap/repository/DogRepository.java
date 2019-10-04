815f7fa182f95567c67869e5eba1a5342423f552
lingxueli <ll2871@columbia.edu>
2019-10-03 22:50:53 -0600

    remove lines not related to the fix

--- src/main/java/com/example/bootstrap/repository/DogRepository.java
+++ src/main/java/com/example/bootstrap/repository/DogRepository.java
@@ -3,10 +3,8 @@ package com.example.bootstrap.repository;
 import com.example.bootstrap.entity.Dog;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Repository;
 
 import java.util.List;
-@Repository
 public interface DogRepository extends CrudRepository<Dog, Long> {
     @Query("select d.breed from Dog d where d.id=:id")
     String findBreedById(Long id);
